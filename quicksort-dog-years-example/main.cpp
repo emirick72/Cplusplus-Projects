cacac0c026803359b00a16391ecf61615b3c2622
Emily Rick <emrick.2261@gmail.com>
2025-07-29 14:09:30 -0400

    Finally finished this example :)

--- quicksort-dog-years-example/main.cpp
+++ quicksort-dog-years-example/main.cpp
@@ -4,38 +4,22 @@
   
   For practice only.
   
-  Date: July 29, 2025
-  
-  -----------------------------------------------------
-  TODO: SEPARATE THE FUNCTION DECLARATIONS AND DEFINITIONS INTO DIFFERENT FILES
-  AND MAKE A NEW .VSCODE TASKS JSON FILE TO COMPILE AND LINK IT ALL TOGETHER.
-  
+  Date: July 29, 2025  
   
   */
 
 
 #include <iostream>
+#include "quicksort.h"
 
 using namespace std;
-
-// Quicksort wrapper function declaration
-void quicksort(int array[], int array_length);
-
-// Recursion function declaration
-void quicksort_recursion(int array[], int low_index, int high_index);
-
-// Partition function declaration
-int partition(int array[], int low_index, int high_index);
-
-
-//-----------------------------------------------
 
 
 int main(){
     int years[] = {2018, 2015, 2010, 2016, 2012};
     int array_length = 5;
 
-    string dog_names[] = {"Nimitz", "Susie", "Clyde", "NuNu", "Walter"};
+    string dog_names[] = {"Noodle", "Chewzers", "Hot Dog", "Piggy", "Wooly Bear"};
     string dog_breeds[] = {"Shih Tzu Schnauzer", "Mini Schnauzer", 
       "Mini Schnauzer/Dachshund mix", "Bichon Frise", "Wheaten Terrier"};
 
@@ -55,40 +39,3 @@ int main(){
     return 0;
 }
 
-//-----------------------------------------------
-
-// Quicksort wrapper function definition
-void quicksort(int array[], int array_length)
-{
-    quicksort_recursion(array, 0, array_length - 1);
-}
-
-// Recursion function definition
-void quicksort_recursion(int array[], int low_index, int high_index)
-{
-    if (low_index < high_index)
-    {
-        int pivot_index = partition(array, low_index, high_index);
-        quicksort_recursion(array, low_index, pivot_index - 1);
-        quicksort_recursion(array, pivot_index + 1, high_index);
-    }
-}
-
-// Partition function definition
-int partition(int array[], int low_index, int high_index)
-{
-    // do something here lol
-    int pivot_value = array[high_index];
-    int i = low_index;
-    for (int j = low_index; j < high_index; j++)
-    {
-        if (array[j] <= pivot_value)
-        {
-            swap(array[i], array[j]);
-            i++;
-        }
-    }
-    swap(array[i], array[high_index]);
-
-    return i;
-}
--- quicksort-dog-years-example/quicksort.h
+++ quicksort-dog-years-example/quicksort.h
@@ -1,13 +1,13 @@
-    // #ifndef QUICKSORT_H
-    // #define QUICKSORT_H
+    #ifndef QUICKSORT_H
+    #define QUICKSORT_H
 
-    // // Quicksort wrapper function declaration
-    // void quicksort(int array[], int array_length);
+    // Quicksort wrapper function declaration
+    void quicksort(int array[], int array_length);
 
-    // // Recursion function declaration
-    // void quicksort_recursion(int array[], int low_index, int high_index);
+    // Recursion function declaration
+    void quicksort_recursion(int array[], int low_index, int high_index);
 
-    // // Partition function declaration
-    // int partition(int array[], int low_index, int high_index);
+    // Partition function declaration
+    int partition(int array[], int low_index, int high_index);
 
-    // #endif // QUICKSORT_H
+    #endif // QUICKSORT_H
